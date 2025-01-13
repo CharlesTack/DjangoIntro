cd0cdd5984116e7d01b5700e64fe6e1327610dc1
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-13 12:37:08 +0000

    amend allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0u+j%eh0dk=ss&w6y_*_016flvg#^6%m(rju+eo$s^#e4c&79&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com', '.herohuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
